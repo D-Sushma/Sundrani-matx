6b1992ff65bcf84a4e88a27cfce63a42ac797e05
D-Sushma <99126858+D-Sushma@users.noreply.github.com>
2023-04-07 19:07:36 +0530

    apply css in form

--- src/app/views/channel-list/shared/OnMainVideo.jsx
+++ src/app/views/channel-list/shared/OnMainVideo.jsx
@@ -72,11 +72,11 @@ const SimpleForm = () => {
         </Box>
 
         <SimpleCard title="Video Details">
-          <Box sx={{width:"60%"}}>
+          <Box sx={{ width: '60%', ml: 30 }}>
             <ValidatorForm onSubmit={handleSubmit} onError={() => null}>
-              <Grid container spacing={6}>
+              <Grid container spacing={8}>
                 <Grid item lg={12} md={6} sm={12} xs={12} sx={{ mt: 2, ml: 5, mr: 5 }}>
-                  <InputLabel sx={{ color: 'blue' }}>
+                  <InputLabel sx={{ color: 'blue', fontWeight: 'bold', fontSize: '15px' }}>
                     Video Name:<span style={{ color: 'red' }}>*</span>
                   </InputLabel>
                   <TextField
@@ -90,19 +90,22 @@ const SimpleForm = () => {
                     label="Enter Video Name"
                     validators={['required', 'minStringLength: 4', 'maxStringLength: 9']}
                   />
-                  <InputLabel sx={{ color: 'blue' }}>Video Description:</InputLabel>
+                  <InputLabel sx={{ color: 'blue', fontWeight: 'bold', fontSize: '15px' }}>
+                    Video Description:
+                  </InputLabel>
                   <TextField
                     type="text"
                     name="firstName"
-                    //   label="First Name"
                     label="Enter Video Description"
                     onChange={handleChange}
                     value={firstName || ''}
                     validators={['required']}
                     errorMessages={['this field is required']}
                   />
-                  <hr style={{ color: 'lightgray' }} />
-                  <InputLabel sx={{ color: 'blue' }}>Video Status:</InputLabel>
+                  {/* <hr style={{ color: 'lightgray' }} /> */}
+                  <InputLabel sx={{ color: 'blue', fontWeight: 'bold', fontSize: '15px' }}>
+                    Video Status:
+                  </InputLabel>
                   <TextField
                     type="text"
                     name="email"
@@ -113,52 +116,36 @@ const SimpleForm = () => {
                     validators={['required', 'isEmail']}
                     errorMessages={['this field is required', 'email is not valid']}
                   />
-                  <hr
-                    style={{
-                      // background: 'lime',
-                      color: 'lightgray'
-                      // borderColor: 'lime',
-                      // height: '3px'
-                    }}
-                  />
-                  {/* <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <DatePicker
-                value={date}
-                onChange={handleDateChange}
-                renderInput={(props) => (
-                  <TextField
-                    {...props}
-                    label="Date picker"
-                    id="mui-pickers-date"
-                    sx={{ mb: 2, width: "100%" }}
-                  />
-                )}
-              />
-            </LocalizationProvider> */}
-                  {/* <TextField
-              sx={{ mb: 4 }}
-              type="number"
-              name="creditCard"
-              label="Credit Card"
-              onChange={handleChange}
-              value={creditCard || ""}
-              errorMessages={["this field is required"]}
-              validators={["required", "minStringLength:16", "maxStringLength: 16"]}
-            /> */}
+                  {/* <hr /> */}
                 </Grid>
-                <Grid direction="row" item md={12} style={{ display: 'flex', paddingTop: 0 }}>
-                  <InputLabel sx={{ color: 'blue' }}>
-                    Video:<span style={{ color: 'red' }}>*</span>
-                  </InputLabel>
-                  <TextField fullWidth autoFocus id="name" type="file" margin="dense" />
-                  <InputLabel sx={{ color: 'blue' }}>
-                    Video Image:<span style={{ color: 'red' }}>*</span>
-                  </InputLabel>
-                  <TextField fullWidth autoFocus id="name" type="file" margin="dense" />
+              </Grid>
+              <Grid container spacing={6}>
+                <Grid
+                  item
+                  lg={12}
+                  md={6}
+                  sm={12}
+                  xs={12}
+                  style={{ display: 'flex', justifyContent: 'space-between', ml: 10 }}
+                >
+                  <Grid sx={{ ml: 5, width: '350px' }}>
+                    <InputLabel sx={{ color: 'blue', fontWeight: 'bold', fontSize: '15px' }}>
+                      Video:<span style={{ color: 'red' }}>*</span>
+                    </InputLabel>
+                    <TextField fullWidth autoFocus id="name" type="file" margin="dense" />
+                  </Grid>
+                  <Grid sx={{ mr: 5, width: '350px' }}>
+                    <InputLabel sx={{ color: 'blue', fontWeight: 'bold', fontSize: '15px' }}>
+                      Video Image:<span style={{ color: 'red' }}>*</span>
+                    </InputLabel>
+                    <TextField fullWidth autoFocus id="name" type="file" margin="dense" />
+                  </Grid>
                 </Grid>
               </Grid>
 
-              <Button color="primary" variant="contained" type="submit">
+              <br />
+              <br />
+              <Button color="primary" variant="contained" type="submit" sx={{ ml: 40 }}>
                 <Icon>send</Icon>
                 <Span sx={{ pl: 1, textTransform: 'capitalize' }}>Submit</Span>
               </Button>
